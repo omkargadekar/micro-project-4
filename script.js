af09f8d9fc6180a326f24bd6a027437a89176107
Omkar <105555236+omkargadekar@users.noreply.github.com>
2023-09-05 13:39:05 +0530

    added

---
+++ script.js
@@ -0,0 +1,22 @@
+// JavaScript functions
+function addToDisplay(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  try {
+    const result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
+}
+
+function backspace() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
